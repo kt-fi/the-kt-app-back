0f428a40d9b43a311bad9130026bbc13ba651f62
Chris Hillman <kt-five@hotmail.com>
2026-07-09 15:52:28 +0200

    general update commit

--- controllers/authController/passwordResetEmail.js
+++ controllers/authController/passwordResetEmail.js
@@ -1,38 +1,57 @@
-import { Resend } from 'resend';
-import { uuid } from 'uuidv4';
-import ResetPasswordToken from '../../schemas/resetPasswordTokenSchema.js';
+import { Resend } from "resend";
+import { uuid } from "uuidv4";
+import ResetPasswordToken from "../../schemas/resetPasswordTokenSchema.js";
+import User from "../../schemas/userSchema.js";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const passwordResetEmail = async (req, res, next) => {
 
-
   let token = uuid();
+  let userId = req.body.userId; // Assuming the user ID is sent in the request body
+  let user;
+  let tokenExists;
 
   console.log(token);
 
   try {
-  
+
+    
+    user = await User.findOne({ _id: userId });
+
+    tokenExists = await ResetPasswordToken.findOne({ userId: userId });
+
+    if (tokenExists) {
+      await ResetPasswordToken.deleteOne({ userId: userId });
+      // res.json({ message: "Existing token deleted. A new token will be created." });
+    }
+
     let resetPasswordToken = await new ResetPasswordToken({
-      userId: '69f1f8f14c83e279035fd748', // Replace with the actual user ID
+      userId: userId,
       token: token,
     });
     await resetPasswordToken.save();
 
-const { data } = await resend.emails.send({
-  from: 'onboarding@resend.dev',
-  to: 'kt-five@hotmail.com',
-  replyTo:  'onboarding@resend.dev',
-  subject: 'hello world',
-  html: '<strong>it works!</strong>',
-});
+
+    const { data } = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: user.email,
+      replyTo: "onboarding@resend.dev",
+      subject: "Reset Password",
+      html: `<strong>Reset your password, click this link</strong> <br> <a href=${process.env.WEB_APP_URL}/reset-password/token'>Reset Password</a>`,
+    });
 
     console.log(data);
-    return res.json({ message: 'Password reset email sent successfully', resetPasswordToken });
+    return res.json({
+      message: "Password reset email sent successfully",
+      resetPasswordToken,
+    });
   } catch (error) {
-    console.error('Failed to send password reset email:', error);
-    return res.status(500).json({ message: 'Failed to send password reset email' });
+    console.error("Failed to send password reset email:", error);
+    return res
+      .status(500)
+      .json({ message: "Failed to send password reset email" });
   }
 };
 
-export default passwordResetEmail;  
+export default passwordResetEmail;
